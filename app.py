d6956c27977ea21ce4f7bc96d614f129d0890b24
Justin Ryan Isidro <106646031+Crific@users.noreply.github.com>
2026-04-06 17:44:00 +0800

    feat: added user login route

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
 # Initialize the Flask application
 app = Flask(__name__)
 
-# Home route (returns health check)
+# Home route 
 @app.route("/")
 def home():
-    return "Portal is running"
+    return render_template("home.html") 
+
+# Login Page Route
+@app.route("/login")
+def login():
+    return render_template("login.html")
 
 # Run app only when executed directly
 if __name__ == "__main__":
