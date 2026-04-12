1442c9ed3c12492419e36adc878fd719b804cd5a
Justin I. <106646031+Crific@users.noreply.github.com>
2026-04-12 08:33:11 +0800

    feat: set up SQLAlchemy and add user model

--- app.py
+++ app.py
@@ -3,8 +3,8 @@
 # =========================
 
 # Phase 1: Database Setup
-# - [ ] Set up database (SQLAlchemy)
-# - [ ] Create User model
+# - [x] Set up database (SQLAlchemy)
+# - [x] Create User model
 # - [ ] Create Request model
 # - [ ] Run db.create_all()
 
@@ -29,9 +29,29 @@
 # Refactor into folders (models, routes) later.
 
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 # Initialize the Flask application
 app = Flask(__name__)
+
+# Flask DB
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+# Initializing db
+db = SQLAlchemy(app)  
+
+# User class
+class User(db.Model):            
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False) # 200 for hash
+    role = db.Column(db.String(20), default="user") 
+
+
+    def __repr__(self):
+        return f"<User {self.username}>"
 
 # Home route 
 @app.route("/")
